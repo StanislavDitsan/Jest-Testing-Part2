4b3370955863d1bb8c45b99d189cd729bfd3c2d4
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-11-07 15:41:56 +0000

    Testing our First Function - Part 2

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -5,7 +5,9 @@
  * @jest-environment jsdom
  */
 
-const {game} = require("../game");
+const {
+    game
+} = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -28,5 +30,8 @@ describe("game object contains correct keys", () => {
     });
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 });
