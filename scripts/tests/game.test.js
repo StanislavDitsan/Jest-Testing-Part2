a5b77f08c775e00d24fff1a05701f68befef8908
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-11-07 16:04:46 +0000

    Adding The newGame Function

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -5,8 +5,11 @@
  * @jest-environment jsdom
  */
 
+
 const {
-    game
+    game,
+    newGame,
+    showScore
 } = require("../game");
 
 beforeAll(() => {
@@ -17,6 +20,7 @@ beforeAll(() => {
     document.close();
 });
 
+// Game object
 
 describe("game object contains correct keys", () => {
     test("score key exists", () => {
@@ -33,5 +37,27 @@ describe("game object contains correct keys", () => {
     });
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+});
+
+describe("newGame works correctly", () => {
+    beforeAll(() => {
+        game.score = 42;
+        game.playerMoves = ["button1", "button2"];
+        game.currentGame = ["button1", "button2"];
+        document.getElementById("score").innerText = "42";
+        newGame();
+    });
+    test("should set game score to zero", () => {
+        expect(game.score).toEqual(0);
+    });
+    test("should clear the computer sequence array", () => {
+        expect(game.currentGame.length).toEqual(0);
+    });
+    test("should clear the player moves array", () => {
+        expect(game.playerMoves.length).toEqual(0);
+    });
+    test("should display 0 for the element with id of score", () => {
+        expect(document.getElementById("score").innerText).toEqual(0);
     });
 });
