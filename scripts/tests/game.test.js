cab8897474edf51044390b45005a9b7d26bcad6a
STANNIDITSANNI <Ditsanstas@gmail.com>
2022-11-08 19:11:29 +0000

    Using Game State

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -11,7 +11,8 @@ const {
     newGame,
     showScore,
     addTurn,
-    lightsOn
+    lightsOn,
+    showTurns
 } = require("../game");
 
 beforeAll(() => {
@@ -39,6 +40,9 @@ describe("game object contains correct keys", () => {
     });
     test("choices contain correct ids", () => {
         expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
+    test("turnNumber key exists", () => {
+        expect("turnNumber" in game).toBe(true);
     });
 });
 
@@ -84,5 +88,10 @@ describe("game play works correctly", () => {
         let button = document.getElementById(game.currentGame[0]);
         lightsOn(game.currentGame[0]);
         expect(button.classList).toContain("light");
+    });
+    test("showTurns should update game.turnNumber",() => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
     });
 });
